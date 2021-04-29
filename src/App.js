477adade10898cd2c668cbff01cb784a62905697
hafidi-alaoui <ayoub.hafidi-alaoui@ecole.ensicaen.fr>
2021-04-29 04:32:07 +0200

    add some features

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Mail from './Mail';
+import EmailList from './EmailList';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Header/>
         <div className="app__body">
           <SideBar/>
           <Switch>
-            
+            <Route path="mail">
+              <Mail/>
+            </Route>
+            <Route path="/">
+                <EmailList/>
+            </Route>
           </Switch>
         </div>
       </div>
